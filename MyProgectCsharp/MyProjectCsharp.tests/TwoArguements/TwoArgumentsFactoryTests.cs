c57c0733694a3400f25c4b19b0a431bfa191a5d1
mihaillo768 <miha76587@gmail.com>
2019-07-20 11:59:28 +0600

    Add Two arguments factory tests.

---
+++ MyProgectCsharp/MyProjectCsharp.tests/TwoArguements/TwoArgumentsFactoryTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyProjectCsharp.TwoArguements;
+using NUnit.Framework;
+
+namespace MyProjectCsharp.tests.TwoArguements
+{
+    [TestFixture]
+    public class TwoArgumentsFactoryTests
+    {
+        [TestCase("MinValue", typeof(MinNumber))]
+        [TestCase("NegPower", typeof(NegPower))]
+        [TestCase("LogOfTwoArg", typeof(LogOfTwoArguments))]
+        [TestCase("PowerTwoArguments", typeof(TwoArgumentsPower))]
+        [TestCase("MaxValue", typeof(MaxNumber))]
+        [TestCase("Remainder", typeof(Remainder))]
+        [TestCase("Plus", typeof(Adder))]
+        [TestCase("Minus", typeof(Minus))]
+        [TestCase("Multiplier", typeof(Multiplier))]
+        [TestCase("Divider", typeof(Divider))]
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = TwoArgumentFactory.CreateCalculator(name);
+
+            Assert.IsInstanceOf(type, calculator);
+        }
+
+    }
+}
